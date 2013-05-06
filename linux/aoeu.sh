dbd8813545cb3b97fb286406156779049919d9ca
shenjia <zshenjia@gmail.com>
2013-05-06 23:22:01 +0800

    in -> is

--- linux/aoeu.sh
+++ linux/aoeu.sh
@@ -30,7 +30,7 @@ alias un='sudo yum remove'   	# uninstall a package
 # Dealing with packages ( for APT in Ubuntu / Debian / ... )
 
 #alias se='sudo apt-cache search' # search a package 
-#alias in='sudo apt-get install'  # install a package
+#alias is='sudo apt-get install'  # install a package
 #alias un='sudo apt-get remove'   # uninstall a package
 
 # Dealing with system
