4c30dd49193057cef3b5f9f3fe344bc07579bfec
shenjia <zshenjia@gmail.com>
2013-11-29 00:10:42 +0800

    multiple alias added

--- mac/plugins/git.sh
+++ mac/plugins/git.sh
@@ -1,38 +1,82 @@
 alias ge='vim /Users/zhangshenjia/Cloud/works/github/aoeu.sh/mac/plugins/git.sh'
 alias gs='git status'
-alias gm='git merge'
+alias gm='git merge --no-ff'
 alias gp='git push'
+alias gpo='git push origin'
+alias gu='git pull'
 alias ga='git add'
+alias gaa='git add -A'
 alias gg='git grep -n'
 alias ggn='git grep --name-only'
 alias gb='git branch'
+alias gba='git branch -a'
 alias gbd='git branch -D'
+alias gbD='git branch -D'
+alias gbv='git branch -v'
 alias gbl='git blame'
 alias gt='git tag'
+alias gta='git tag -a'
+alias gtn='git tag -n10'
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gdh='git diff HEAD'
 alias gl='git log --pretty=format:"%h: %s (%an, %ar)" --graph'
+alias grl='git reflog'
 alias gr='git reset --hard'
 alias grh='git reset --hard HEAD'
+alias grm='git reset --hard master'
+alias grom='git reset --hard origin/master'
+alias grb='git rebase'
 alias gv='git revert'
 alias gvh='git revert HEAD'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcaa='git commit -a --amend'
 alias go='git checkout'
+alias gom='git checkout master'
+alias god='git checkout develop'
+alias gob='git checkout -b'
+alias goh='git checkout HEAD'
 alias gst='git stash'
 alias gsta='git stash apply'
 alias gstl='git stash list'
 alias gstc='git stash clear'
 alias gi='git bisect'
-alias gf='git fsck --lost-found'
+alias gf='git fetch'
+alias gfo='git fetch origin'
+alias gfl='git fsck --lost-found'
+alias grm='git rm'
+alias grmc='git rm --cached'
 alias gsm='git submodule'
 alias gx='gitx'
+alias ged='echo "!.gitignore" > .gitignore'
+alias gw='git show'
+function gtm() {
+	TAG=$1
+	shift
+	echo $TAG -m "\"$@\"" | xargs git tag
+}
+function gcm() {
+	FILE=$1
+    shift
+	echo $FILE -m "\"$@\"" | xargs git commit
+}
 function gcam() {
-	git commit -a -m "$@"
+	echo "\"$@\"" | xargs git commit -a -m
 }
 function gcp() {
-	git commit -a -m "$@" && git push
+	echo "\"$@\"" | xargs git commit -a -m && git push
+}
+function ginit() {
+	git init
+    echo ".DS_Store"  >  .gitignore
+    echo ".buildpath" >> .gitignore
+    echo ".project"   >> .gitignore
+    echo ".settings"  >> .gitignore
+	git add -A
+	git commit -a -m init
+	git tag init -m "init repo"  
+	git checkout -b develop
 }
 function ginfo() {
     pushd . >/dev/null
@@ -62,7 +106,7 @@ function ginfo() {
 source ~/.git-prompt.sh
 COLOR_ID="\[\e[1;30m\]"
 COLOR_PWD='\[\e[m\]'
-COLOR_GIT='\[\e[1;32m\]'
+COLOR_GIT='\[\e[1;34m\]'
 COLOR_SPLITTER='\[\e[1;34m\]'
 COLOR_PROMPT='\[\e[m\]'
 SPLITTER="$ "
