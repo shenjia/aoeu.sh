658503708c304e63e6d9b6d7f51f32088bfc196a
shenjia <zshenjia@gmail.com>
2013-11-29 00:11:07 +0800

    yiic changed

--- mac/plugins/php.sh
+++ mac/plugins/php.sh
@@ -1,4 +1,5 @@
 alias nr='sudo nginx -s reload'
 alias pr='re php54-fpm'
 alias cpd='/Users/zhangshenjia/Documents/software/web/php/phpcpd.phar'
-alias yc='./protected/yiic'
+alias yc='./yiic'
+alias ys='./yiic shell www/index.php'
