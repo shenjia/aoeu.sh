b87d003a93cde88d8d41ee28af133bf9e191f8de
shenjia <zshenjia@gmail.com>
2014-02-13 16:35:56 +0800

    yii stuff

--- mac/plugins/php.sh
+++ mac/plugins/php.sh
@@ -1,5 +1,3 @@
 alias nr='sudo nginx -s reload'
 alias pr='re php54-fpm'
 alias cpd='/Users/zhangshenjia/Documents/software/web/php/phpcpd.phar'
-alias yc='./yiic'
-alias ys='./yiic shell www/index.php'
---
+++ mac/plugins/yii.sh
@@ -0,0 +1,2 @@
+alias yc='./yiic'
+alias ys='./yiic shell www/index.php'
