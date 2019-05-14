b12f8b0bd2bbc4dbebacc9edd45270d7cf583135
shenjia <zshenjia@gmail.com>
2019-05-15 05:34:33 +0800

    brew

--- mac/plugins/brew.sh
+++ mac/plugins/brew.sh
@@ -5,3 +5,15 @@ alias bi='brew install'      # install a package
 alias biv='brew -v install'  # install a package
 alias bl='brew list'         # list installed packages
 alias bu='brew uninstall'    # uninstall a package
+function autoload() {
+    cp $1 ~/Library/LaunchAgents/
+    launchctl load -w ~/Library/LaunchAgents/$1
+    echo "Added to LaunchAgents!!"
+}
+function reload() {
+    echo "Stopping [ $1 ] ..."
+    launchctl unload -w $2
+    echo "Starting [ $1 ] ..."
+    launchctl load -w $2
+    echo "[ $1 ] restarted."
+}
--- mac/plugins/php.sh
+++ mac/plugins/php.sh
@@ -1,6 +1,9 @@
-alias php='/usr/local/opt/php@7.0/bin/php'
-alias nr='sudo nginx -s reload'
-alias pr='re php54-fpm'
 alias cpd='/Users/zhangshenjia/Documents/software/web/php/phpcpd.phar'
 alias te='e /Users/zhangshenjia/test/test.php'
 alias tt='php /Users/zhangshenjia/test/test.php'
+function pr() {
+    reload php-fpm ~/Library/LaunchAgents/homebrew.mxcl.php\@7.1.plist
+}
+function nr() {
+    reload nginx ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
+}
